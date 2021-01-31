b0b19a6457a2c41b431146fd8ba7450cbbae5f4c
lovemikael <74554026+lovemikael@users.noreply.github.com>
2021-01-31 15:18:15 +0100

    Add files via upload

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,11 @@
 import "@babylonjs/core/Debug/debugLayer";
 import "@babylonjs/inspector";
 import "@babylonjs/loaders/glTF";
-import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, FreeCamera, WebXRExperienceHelper,WebXRFeaturesManager,WebXRPlaneDetector,WebXRAnchorSystem, Color3, HingeJoint, StandardMaterial } from "@babylonjs/core";
+import { Engine, Scene, ArcRotateCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, FreeCamera, WebXRExperienceHelper,WebXRFeaturesManager,WebXRPlaneDetector,WebXRAnchorSystem, Color3, HingeJoint, StandardMaterial, SceneLoader } from "@babylonjs/core";
 import {HelloWorld} from "./classes/HelloWorld";
 import { version } from "webpack";
 class App{
-    
     constructor(){
-        
-
-
 
         var createScene = async function name() {
             var scene = new Scene(engine);
@@ -20,14 +16,22 @@ class App{
             var light1: HemisphericLight = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
             light1.intensity = 0.5;
 
+            
+            // SceneLoader.Append("https://raw.githubusercontent.com/lovemikael/Multiplayer/main/","adamHead.gltf",scene,function () {
+            //     console.log("Mesh loaded.");
+            // });
             var sphere: Mesh = Mesh.CreateSphere("spere1",16,0.5,scene);
+            var box: Mesh = Mesh.CreateBox("box",0.5,scene);
+            box.position.y = 1;
+            box.position.z = 1;
             
             sphere.position.y = 1.5;
             sphere.position.z = 1.5;
             var boxMaterial = new StandardMaterial("boxmaterial",scene);
-            boxMaterial.diffuseColor = Color3.Purple();
+            boxMaterial.diffuseColor = Color3.Red();
             sphere.material = boxMaterial;
 
+            
             const xr = await scene.createDefaultXRExperienceAsync({
                 uiOptions: {sessionMode: "immersive-ar"}
             });
@@ -36,14 +40,10 @@ class App{
             
             return scene;
         }
-        
-        
-        
+
         var canvas = <HTMLCanvasElement>document.getElementById('renderCanvas');
         var engine = new Engine(canvas, true);
         
-        
-
         createScene().then(scene => {
             engine.runRenderLoop(() => scene.render());
         })
